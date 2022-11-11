f248b0628d76fa03333ef41dd816831d61f0b0ba
Tashtan <satievtashtan@gmail.com>
2022-11-11 08:40:42 +0300

    token renewed

--- lib/constants/credentials.dart
+++ lib/constants/credentials.dart
@@ -1,7 +1,7 @@
 const String getUsersEndpoint = 'https://api.github.com/users';
 const int perPage = 20;
 
-const String token = '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';
+const String token = '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';
 const Map<String, String> headers = {
   'Content-Type': "application/json",
   'Authorization': 'bearer ${token}'
